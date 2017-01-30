e2a48649335f4ba3d45bd07833bcee8bad05c09c
Curtis Jackson <jacksonc4@vcu.edu>
2017-01-30 16:19:33 -0500

    Error fixing
    Errors existed in consul_install.sh and docker_install.sh. Updated to
    fix.

---
+++ shell_scripts/consul_install.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+#Installs consul for service discovery and node monitoring,
+#and then creates a new agent for the node.
+
+cd
+
+#updates packages and installs unzip function
+sudo apt-get update
+sudo apt-get install unzip
+
+#switch to bin directory and get .zip for consul
+cd /usr/local/bin
+sudo wget https://releases.hashicorp.com/consul/0.7.3/consul_0.7.3_linux_amd64.zip
+
+#unzip downloaded file and remove .zip file
+sudo unzip *.zip
+sudo rm *.zip
+
+#grab user input for host IP
+echo ***Input host IP for consul agent configuration***
+read instance_IP
+
+#initialize consul agent on node
+sudo consul agent -server -data-dir="/tmp/consul" -bootstrap -advertise=“$instance_IP” -http-port=8500 -client=0.0.0.0 &
---
+++ shell_scripts/docker_install.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+#Installs and starts Docker-engine
+
+cd
+
+#update packages
+sudo apt-get update
+
+#ensure apt works with https method and that CA certificates are installed
+sudo apt-get install apt-transport-https ca-certificates
+
+#add GPG encryption key
+sudo apt-key adv -keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
+
+#Create new docker.list file and add the ubuntu-xenial image to it
+#Add in the following ubuntu image link for docker to use: 
+#deb https://apt.dockerproject.org/repo ubuntu-xenial main
+sudo vi /etc/apt/sources.list.d/docker.list
+
+#update packages again
+sudo apt-get update 
+
+#purge the old repo if it exists, and verify the cache is pulling
+#from the right repository
+sudo apt-get purge lxc-docker 
+sudo apt-cache policy docker-engine
+
+#install recommended pre-requisites for OS
+sudo apt-get install linux-image-extra$(uname -r)
+
+#update packages again and install docker-engine
+sudo apt-get update
+sudo apt-get install docker-engine
+
+#update packages again and install docker-engine
+sudo service docker start
