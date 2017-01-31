053e0b3958f1e1784de685e2cd5982497368956b
jacksonc4 <jacksonc4@vcu.edu>
2017-01-31 00:12:39 -0500

    Update docker_install.sh

--- shell_scripts/docker_install.sh
+++ shell_scripts/docker_install.sh
@@ -6,31 +6,25 @@ cd
 #update packages
 sudo apt-get update
 
+#make sure that curl is installed, and get recommended pre-requisites for OS
+sudo apt-get install curl \
+    linux-image-extra-$(uname -r) \
+    linux-image-extra-virtual
+
 #ensure apt works with https method and that CA certificates are installed
 sudo apt-get install apt-transport-https ca-certificates
 
-#add GPG encryption key
-sudo apt-key adv -keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
+#add official docker GPG encryption key
+curl -fsSL https://yum.dockerproject.org/gpg | sudo apt-key add -
 
-#Create new docker.list file and add the ubuntu-xenial image to it
-#Add in the following ubuntu image link for docker to use: 
-#deb https://apt.dockerproject.org/repo ubuntu-xenial main
-sudo vi /etc/apt/sources.list.d/docker.list
+#verify the correct key ID is 58118E89F3A912897C070ADBF76221572C52609D
+apt-key fingerprint 58118E89F3A912897C070ADBF76221572C52609D
 
-#update packages again
-sudo apt-get update 
-
-#purge the old repo if it exists, and verify the cache is pulling
-#from the right repository
-sudo apt-get purge lxc-docker 
-sudo apt-cache policy docker-engine
-
-#install recommended pre-requisites for OS
-sudo apt-get install linux-image-extra$(uname -r)
+#Set up the stable docker repository
+sudo add-apt-repository "deb https://apt.dockerproject.org/repo/ ubuntu-$(lsb_release -cs) main"
 
 #update packages again and install docker-engine
 sudo apt-get update
-sudo apt-get install docker-engine
+sudo apt-get -y install docker-engine
 
-#update packages again and install docker-engine
-sudo service docker start
+echo Docker is running.
