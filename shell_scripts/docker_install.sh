120bf1a1b1ff31e3aa90154323473720537b3755
jacksonc4 <jacksonc4@vcu.edu>
2017-02-01 10:56:39 -0500

    Update docker_install.sh

--- shell_scripts/docker_install.sh
+++ shell_scripts/docker_install.sh
@@ -24,11 +24,3 @@ sudo add-apt-repository "deb https://apt.dockerproject.org/repo/ ubuntu-$(lsb_re
 #update packages again and install docker-engine
 sudo apt-get update
 sudo apt-get -y install docker-engine
-
-#Download the Docker Machine binary and extract it to your file path
-curl -L https://github.com/docker/machine/releases/download/v0.9.0/docker-machine-`uname -s`-`uname -m` >/tmp/docker-machine &&
-  chmod +x /tmp/docker-machine &&
-  sudo cp /tmp/docker-machine /usr/local/bin/docker-machine
-
-#Check the installation by displaying the Machine version
-sudo docker-machine version
