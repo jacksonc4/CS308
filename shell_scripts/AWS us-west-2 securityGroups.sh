a23e3d561889ef8177bec16ec1841ffb3ca5331b
jacksonc4 <jacksonc4@vcu.edu>
2017-03-18 09:56:12 -0400

    Run on local machine to create needed groups

---
+++ shell_scripts/AWS us-west-2 securityGroups.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+#Creates necessary AWS Security Groups in us-west-2
+
+#Set up general rules
+aws ec2 --region us-west-2 create-security-group --group-name General --description “Standard access ports“
+  aws ec2 --region us-west-2 authorize-security-group-ingress --group-name General --protocol tcp --port 80 --cidr 0.0.0.0/0
+  aws ec2 --region us-west-2 authorize-security-group-ingress --group-name General --protocol tcp --port 22 --cidr 0.0.0.0/0
+
+#Sets up Cassandra rules
+aws ec2 --region us-west-2 create-security-group --group-name Cassandra --description “Cassandra communication ports“
+  aws ec2 --region us-west-2 authorize-security-group-ingress --group-name Cassandra --protocol tcp --port 9162 --cidr 0.0.0.0/0
+  aws ec2 --region us-west-2 authorize-security-group-ingress --group-name Cassandra --protocol tcp --port 9042 --cidr 0.0.0.0/0
+  aws ec2 --region us-west-2 authorize-security-group-ingress --group-name Cassandra --protocol tcp --port 7199 --cidr 0.0.0.0/0
+  aws ec2 --region us-west-2 authorize-security-group-ingress --group-name Cassandra --protocol tcp --port 7001 --cidr 0.0.0.0/0
+  aws ec2 --region us-west-2 authorize-security-group-ingress --group-name Cassandra --protocol tcp --port 7000 --cidr 0.0.0.0/0
+  aws ec2 --region us-west-2 authorize-security-group-ingress --group-name Cassandra --protocol tcp --port 1024-65355 --cidr 0.0.0.0/0
+
+#Sets up Redis Rules
+aws ec2 --region us-west-2 create-security-group --group-name Redis --description “Redis communications ports“
+  aws ec2 --region us-west-2 authorize-security-group-ingress --group-name Redis --protocol tcp --port 22122 --cidr 0.0.0.0/0
+  aws ec2 --region us-west-2 authorize-security-group-ingress --group-name Redis --protocol tcp --port 16379 --cidr 0.0.0.0/0
+  aws ec2 --region us-west-2 authorize-security-group-ingress --group-name Redis --protocol tcp --port 6379 --cidr 0.0.0.0/0
