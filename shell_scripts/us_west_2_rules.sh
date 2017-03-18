4e509261a1fb03d30b95f75a9650f0db109e0010
Curtis Jackson <jacksonc4@vcu.edu>
2017-03-18 13:02:57 -0400

    Add files via upload

---
+++ shell_scripts/us_west_2_rules.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+#General rules
+aws ec2 --region us-west-2 authorize-security-group-ingress --group-name ClusterTestEnv --protocol tcp --port 80 --cidr 0.0.0.0/0
+aws ec2 --region us-west-2 authorize-security-group-ingress --group-name ClusterTestEnv --protocol tcp --port 22 --cidr 0.0.0.0/0
+
+#Cassandra rules
+aws ec2 --region us-west-2 authorize-security-group-ingress --group-name ClusterTestEnv --protocol tcp --port 9162 --cidr 0.0.0.0/0
+aws ec2 --region us-west-2 authorize-security-group-ingress --group-name ClusterTestEnv --protocol tcp --port 9042 --cidr 0.0.0.0/0
+aws ec2 --region us-west-2 authorize-security-group-ingress --group-name ClusterTestEnv --protocol tcp --port 7199 --cidr 0.0.0.0/0
+aws ec2 --region us-west-2 authorize-security-group-ingress --group-name ClusterTestEnv --protocol tcp --port 7001 --cidr 0.0.0.0/0
+aws ec2 --region us-west-2 authorize-security-group-ingress --group-name ClusterTestEnv --protocol tcp --port 7000 --cidr 0.0.0.0/0
+aws ec2 --region us-west-2 authorize-security-group-ingress --group-name ClusterTestEnv --protocol tcp --port 1024-65355 --cidr 0.0.0.0/0
+
+#Redis rules
+aws ec2 --region us-west-2 authorize-security-group-ingress --group-name ClusterTestEnv --protocol tcp --port 22122 --cidr 0.0.0.0/0
+aws ec2 --region us-west-2 authorize-security-group-ingress --group-name ClusterTestEnv --protocol tcp --port 16379 --cidr 0.0.0.0/0
+aws ec2 --region us-west-2 authorize-security-group-ingress --group-name ClusterTestEnv --protocol tcp --port 6379 --cidr 0.0.0.0/0
+
+echo Rules added.
